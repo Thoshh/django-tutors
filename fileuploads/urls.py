04e1592220264230c63e8557f72c2c122d65f20e
jarek.zgoda <jarek.zgoda@dc7af962-e955-0410-a688-cdf2c3f81c22>
2008-09-02 14:55:25 +0000

    Serving static files for dev server and proper definitions of media components

--- fileuploads/urls.py
+++ fileuploads/urls.py
@@ -3,6 +3,7 @@
 __revision__ = '$Id$'
 
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 from django.contrib import admin
 
@@ -12,3 +13,8 @@ urlpatterns = patterns('',
     (r'^admin/(.*)', admin.site.root),
     (r'^uploads/', include('uploads.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
