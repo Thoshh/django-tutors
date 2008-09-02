4dc2aef748965a0f496ada52cf8661a0f2661b4b
jarek.zgoda <jarek.zgoda@dc7af962-e955-0410-a688-cdf2c3f81c22>
2008-09-02 14:55:25 +0000

    Serving static files for dev server and proper definitions of media components

--- trunk/fileuploads/settings.py
+++ trunk/fileuploads/settings.py
@@ -46,12 +46,12 @@ MEDIA_ROOT = os.path.join(ROOT_PATH, 'media')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/media/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'tp$=x^8xpxe5#8qlf3u@$7m%6=f31*s+_t=b@&bhn7oni%w$l5'
