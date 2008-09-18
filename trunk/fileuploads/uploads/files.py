8a69b33b0fbecc5b2de6409945af0eef3558f5c7
jarek.zgoda <jarek.zgoda@dc7af962-e955-0410-a688-cdf2c3f81c22>
2008-09-18 07:50:21 +0000

    Działający kod do upload handlera (tak, od tej chwili logi do commitów będą po polsku)

--- trunk/fileuploads/uploads/files.py
+++ trunk/fileuploads/uploads/files.py
@@ -8,13 +8,23 @@ from django.core.files.uploadhandler import FileUploadHandler
 
 class LoggingUploadHandler(FileUploadHandler):
 
+    def __init__(self, request=None):
+        self.file_size = None
+        super(LoggingUploadHandler, self).__init__(request)
+
     def handle_raw_input(self, input_data, META, content_length, boundary, encoding=None):
-        self.uploaded = 0
-        self._log_percentage()
+        if self.file_size is None and content_length:
+            self.file_size = float(content_length)
         return None
 
+    def new_file(self, field_name, file_name, content_type, content_length, charset):
+        if self.file_size is None and content_length:
+            self.file_size = float(content_length)
+        self.uploaded = 0.0
+        self._log_percentage()
+
     def receive_data_chunk(self, raw_data, start):
-        self.uploaded = self.uploaded + self.chunk_size
+        self.uploaded = self.uploaded + float(self.chunk_size)
         self._log_percentage()
         return raw_data
 
@@ -23,4 +33,9 @@ class LoggingUploadHandler(FileUploadHandler):
         return None
 
     def _log_percentage(self):
-        print 'uploaded %d bytes' % self.uploaded
+        if self.file_size is None:
+            percentage = 'unknown'
+        else:
+            percents = (self.uploaded / self.file_size) * 100
+            percentage = '%.2f' % percents
+        print 'uploaded %d bytes (%s %%)' % (self.uploaded, percentage)
