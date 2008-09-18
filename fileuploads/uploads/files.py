223774eeba2e389a4d66024d2060415182381bed
jarek.zgoda <jarek.zgoda@dc7af962-e955-0410-a688-cdf2c3f81c22>
2008-09-18 13:20:28 +0000

    Lepszy UploadHandler i ciąg dalszy dokumentacji

--- fileuploads/uploads/files.py
+++ fileuploads/uploads/files.py
@@ -18,10 +18,9 @@ class LoggingUploadHandler(FileUploadHandler):
         return None
 
     def new_file(self, field_name, file_name, content_type, content_length, charset):
-        if self.file_size is None and content_length:
-            self.file_size = float(content_length)
         self.uploaded = 0.0
         self._log_percentage()
+        return None
 
     def receive_data_chunk(self, raw_data, start):
         self.uploaded = self.uploaded + float(self.chunk_size)
@@ -29,7 +28,7 @@ class LoggingUploadHandler(FileUploadHandler):
         return raw_data
 
     def file_complete(self, file_size):
-        self._log_percentage()
+        print 'upload completed'
         return None
 
     def _log_percentage(self):
@@ -37,5 +36,7 @@ class LoggingUploadHandler(FileUploadHandler):
             percentage = 'unknown'
         else:
             percents = (self.uploaded / self.file_size) * 100
+            if percents > 100:
+                percents = 100.0
             percentage = '%.2f' % percents
         print 'uploaded %d bytes (%s %%)' % (self.uploaded, percentage)
