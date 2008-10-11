e6325ff930dc039ca90f9f9009fbc7ad666eb520
jarek.zgoda <jarek.zgoda@dc7af962-e955-0410-a688-cdf2c3f81c22>
2008-10-11 19:38:33 +0000

    Proof of concept code works

--- feeds/publisher/models.py
+++ feeds/publisher/models.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__revision__ = '$Id$'
+
 from django.db import models
 
-# Create your models here.
+class Entry(models.Model):
+    user = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
